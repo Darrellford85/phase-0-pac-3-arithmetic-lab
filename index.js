d1ba245c3261c86c5747ae46cc4121425b1772b0
Darrell Ford <Darrellford85@gmail.com>
2022-03-12 06:58:12 -0800

    Comleted assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){
+  console.log();
+}
+function subtract(){
+  console.log();
+}
+function multiply(){
+  console.log();
+}
+function divide(){
+  console.log();
+}
+function add(a, b){
+  return(a + b);
+}
+function subtract(a, b){
+  return(a - b);
+}
+function multiply(a, b){
+  return(a * b);
+}
+function divide(a, b){
+  return (a / b);
+}
+function increment(n){
+  return n += 1;
+}
+function decrement(n){
+  return n -= 1;
+}
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+ function preserveDecimal(n){
+   return parseFloat(n, 2.222); // 2.222
+ }
